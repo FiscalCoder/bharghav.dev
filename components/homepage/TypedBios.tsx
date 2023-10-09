8bd501a13bf28ba5a82c3c97ea6a93cb61077eb5
Bharghav <bharghav.raman@tarento.com>
2023-10-09 11:27:21 +0530

    chore: optimize bio for better narration

--- components/homepage/TypedBios.tsx
+++ components/homepage/TypedBios.tsx
@@ -31,13 +31,13 @@ export function TypedBios() {
     <div>
       <ul id="bios" className="hidden">
         <li>
-          {tr.t('bio_1')} <Twemoji emoji="man-office-worker" />
+          {tr.t('bio_1')} <Twemoji emoji="man-technologist" />
         </li>
         <li>
-          {tr.t('bio_2')} <Twemoji emoji="briefcase" />
+          {tr.t('bio_2')} <Twemoji emoji="cityscape" />
         </li>
         <li>
-          {tr.t('bio_3')} <Twemoji emoji="calendar" />
+          {tr.t('bio_3')} <Twemoji emoji="briefcase" />
         </li>
         <li>
           {tr.t('bio_4')} <Twemoji emoji="computer" />
@@ -49,30 +49,23 @@ export function TypedBios() {
           {tr.t('bio_6')} <Twemoji emoji="toolbox" />
         </li>
         <li>
-          {tr.t('bio_7')} <Twemoji emoji="bust-in-silhouette" />{' '}
-          <Twemoji emoji="man-office-worker" />
+          {tr.t('bio_7')} <Twemoji emoji="bust-in-silhouette" /> <Twemoji emoji="airplane" />
         </li>
         <li>
-          {tr.t('bio_8')} <Twemoji emoji="rocket" />
-        </li>
-        <li>
-          {tr.t('bio_9')} <Twemoji emoji="earth-asia" /> <Twemoji emoji="man-office-worker" />
-        </li>
-        <li>
-          {tr.t('bio_10')} <Twemoji emoji="cityscape" />
-        </li>
-        <li>
-          {tr.t('bio_11')} <Twemoji emoji="heart" /> <Twemoji emoji="airplane" />
-        </li>
-        <li>
-          {tr.t('bio_12')} <Twemoji emoji="musical-note" /> <Twemoji emoji="violin" />{' '}
+          {tr.t('bio_8')} <Twemoji emoji="musical-note" /> <Twemoji emoji="violin" />{' '}
           <Twemoji emoji="musical-keyboard" />
         </li>
         <li>
-          {tr.t('bio_13')} <Twemoji emoji="video-game" /> <Twemoji emoji="soccer" />
+          {tr.t('bio_9')} <Twemoji emoji="video-game" />
         </li>
         <li>
-          {tr.t('bio_14')} <Twemoji emoji="bicyclist" />
+          {tr.t('bio_10')} <Twemoji emoji="bicyclist" />
+        </li>
+        <li>
+          {tr.t('bio_11')} <Twemoji emoji="calendar" /> <Twemoji emoji="rocket" />
+        </li>
+        <li>
+          {tr.t('bio_12')} <Twemoji emoji="earth-asia" /> <Twemoji emoji="man-office-worker" />
         </li>
       </ul>
       <span ref={el} className="text-neutral-900 dark:text-neutral-200" />
