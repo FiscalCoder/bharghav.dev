91ffada7dedcdfc74e464dfa951e2a95ce8e32a9
Bharghav <bharghav.raman@tarento.com>
2024-03-01 00:43:38 +0530

    fix: mobile viewport zoom

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,6 +33,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500&display=swap"
             rel="stylesheet"
           />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-dark dark:text-white">
           <Main />
